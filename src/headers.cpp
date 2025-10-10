202b7b9754e45bc004bb19fc48fb07013b58fd9b
Vitalii Orazov <viordash@yandex.ru>
2025-10-10 20:06:23 +0000

    headers: implement iterHeaders()

--- src/headers.cpp
+++ src/headers.cpp
@@ -5,10 +5,40 @@
 
 using namespace std::string_view_literals;
 
-using Callback = std::function<void(std::string_view, std::string_view)>;
+static void trim(std::string_view &s) {
+    auto start = s.find_first_not_of(" \t");
+    auto end = s.find_last_not_of(" \t");
+    if (start != std::string_view::npos && end != std::string_view::npos) {
+        s = s.substr(start, end - start + 1);
+    }
+}
 
 void iterHeaders(std::string_view req, Callback &&callback) {
-    // code here
+    size_t request_delimeter = req.find("\r\n");
+    if (request_delimeter == std::string_view::npos) {
+        return;
+    }
+
+    std::string_view headers = req.substr(request_delimeter + 2);
+
+    for (auto line_range : headers | std::views::split("\r\n"sv)) {
+        std::string_view line(line_range.begin(), line_range.end());
+
+        if (line.empty()) {
+            break;
+        }
+
+        size_t colon = line.find(':');
+        if (colon != std::string_view::npos) {
+            std::string_view name = line.substr(0, colon);
+            std::string_view value = line.substr(colon + 1);
+            
+            trim(name);
+            trim(value);
+
+            callback(name, value);
+        }
+    }
 }
 
 std::pair<std::string, std::string> findHostPort(std::string_view req) {
